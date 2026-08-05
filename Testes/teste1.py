d4f57cbc1bd524d2759a8151cf2819ebc0b20f2c
Girley <girleyeliandersonmachado@gmail.com>
2026-08-04 21:06:33 -0300

    começando a limpar os dados

--- Testes/teste1.py
+++ Testes/teste1.py
@@ -3,3 +3,7 @@ import pandas as pd
 df = sns.load_dataset("titanic")
 tabela = pd.DataFrame(df)
 print(tabela.info())
+df = df.drop('deck', axis=1)
+df_sem_null = df.fillna(df.mean(numeric_only=True))
+df_sem_null = df_sem_null.dropna()  # Drop any remaining rows with NaN values
+print(df_sem_null.info())
