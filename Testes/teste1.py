c010021a737fe6abcb0554b799b26736709d3b38
Girley <girleyeliandersonmachado@gmail.com>
2026-09-15 18:10:34 -0300

    Update teste1.py

--- Testes/teste1.py
+++ Testes/teste1.py
@@ -2,8 +2,7 @@ import seaborn as sns
 import pandas as pd
 df = sns.load_dataset("titanic")
 tabela = pd.DataFrame(df)
-print(tabela.info())
 df = df.drop('deck', axis=1)
 df_sem_null = df.fillna(df.mean(numeric_only=True))
 df_sem_null = df_sem_null.dropna()  # Drop any remaining rows with NaN values
-print(df_sem_null.info())
+print(df_sem_null)
